d48f665f4b2610abb4b2fd8e8229010e6b184b33
fungos <fungos@gmail.com>
2012-02-28 21:02:48 -0300

    header copyright changed to reflect new licensing terms

--- include/interface/IEventListener.h
+++ include/interface/IEventListener.h
@@ -1,33 +1,32 @@
-/******************************************************************************
- ** Copyright (c) 2010 Seed Developers
- ** All rights reserved
- ** Contact: licensing@seedframework.org
- ** Website: http://www.seedframework.org
-
- ** This file is part of the Seed Framework.
-
- ** Commercial Usage
- ** Seed Framework is available under proprietary license for those who cannot,
- ** or choose not to, use LGPL and GPL code in their projects (eg. iPhone,
- ** Nintendo Wii and others).
-
- ** GNU Lesser General Public License Usage
- ** Alternatively, this file may be used under the terms of the GNU Lesser
- ** General Public License version 2.1 as published by the Free Software
- ** Foundation and appearing in the file LICENSE.LGPL included in the
- ** packaging of this file.  Please review the following information to
- ** ensure the GNU Lesser General Public License version 2.1 requirements
- ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
- **
- ** In addition, as a special exception, Seed developers gives you certain
- ** additional rights. These rights are described in the Seed Framework LGPL
- ** Exception version 1.1, included in the file LGPL_EXCEPTION.txt in this
- ** package.
- **
- ** If you have questions regarding the use of this file, please contact
- ** Seed developers at licensing@seedframework.org.
- **
- *****************************************************************************/
+/*
+* Copyright (c) 2012, Seed Developers
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. The origin of this software must not be misrepresented; you must not
+*    claim that you wrote the original software.
+* 2. Altered source versions must be plainly marked as such, and must not be
+*    misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+* 4. A credit line is required in the game credits, as the following line:
+*    Powered by Seed Framework
+* 5. A Seed Framework logo should be displayed within a splash screen, or in
+*    the case where the game haven't or cannot have a splash, a logo should be
+*    displayed somewhere in the game (eg. menu, credits, other place).
+*
+* THIS SOFTWARE IS PROVIDED BY SEED DEVELOPERS ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL SEED DEVELOPERS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #ifndef __IEVENT_LISTENER_H__
 #define __IEVENT_LISTENER_H__
