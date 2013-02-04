6e78155e25faf627338368a74aa6ae981b1be356
fungos <fungos@gmail.com>
2013-02-04 21:38:31 -0200

    .

--- samples/rocket/rocket_sample.h
+++ samples/rocket/rocket_sample.h
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-#ifndef _ROCKET_SAMPLE_H_
-#define _ROCKET_SAMPLE_H_
-
-#include <api/rocket/RocketInterface.h>
-#include <Rocket/Core.h>
-#include <Seed.h>
-using namespace Seed;
-using namespace Seed::RocketGui;
-
-extern SceneNode *gScene;
-
-class RocketSample : public IGameApp,
-					public IEventSystemListener,
-					public IEventInputKeyboardListener,
-					public IEventInputPointerListener,
-					public IEventJobListener
-{
-	public:
-		RocketSample();
-		virtual ~RocketSample();
-
-		virtual bool Initialize();
-		virtual bool Update(f32 dt);
-		virtual bool Shutdown();
-
-		// IEventSystemListener
-		virtual void OnSystemShutdown(const EventSystem *ev);
-
-		// IEventInputKeyboardListener
-		virtual void OnInputKeyboardRelease(const EventInputKeyboard *ev);
-
-		// IEventInputPointerListener
-		virtual void OnInputPointerRelease(const EventInputPointer *ev);
-
-		// IEventJobListener
-		virtual void OnJobCompleted(const EventJob *ev);
-		virtual void OnJobAborted(const EventJob *ev);
-
-	private:
-		SEED_DISABLE_COPY(RocketSample);
-
-	protected:
-		ISceneObject *pImage;
-		f32			fElapsed;
-		f32			fDir;
-		bool		bRotate;
-
-		SceneNode	cScene;
-		Viewport	cViewport;
-		Camera		*pCamera;
-		Renderer	cRenderer;
-
-		Vector3f	vFrom;
-		Vector3f	vCurrent;
-		Vector3f	vTo;
-
-		RocketInterface *pI;
-		Rocket::Core::Context *pContext;
-		Rocket::Core::ElementDocument *pDoc;
-};
-
-#endif // _ROCKET_SAMPLE_H_
-=======
 #ifndef _ROCKET_SAMPLE_H_
 #define _ROCKET_SAMPLE_H_
 
@@ -113,4 +49,3 @@ class RocketSample : public IGameApp,
 };
 
 #endif // _ROCKET_SAMPLE_H_
->>>>>>> a9580b74bf80685c754dd1f7b36da15e51452b9b
